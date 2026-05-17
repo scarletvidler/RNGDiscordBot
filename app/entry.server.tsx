1462d3d759c82dbf72c17f862764ee2dc2f74153
Scarlett Vidler <56361191+scarletvidler@users.noreply.github.com>
2026-05-17 21:11:18 +0100

    fix: :bug: Fixed removed dependancies

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -12,8 +12,6 @@ import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
 
-import "./backend/services/bot";
-
 const ABORT_DELAY = 5_000;
 
 export default function handleRequest(
