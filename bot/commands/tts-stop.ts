94506cb00a15f8e5be53aca40621d85ab0a13834
Scarlett Vidler <56361191+scarletvidler@users.noreply.github.com>
2026-06-03 16:17:23 +0100

    Feat: Added a force stop to Lerche for when the API goes nuts.

---
+++ bot/commands/tts-stop.ts
@@ -0,0 +1,32 @@
+import { BotCommand } from "../types";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+
+const command: BotCommand = {
+  data: new SlashCommandBuilder()
+    .setName("tts-stop")
+    .setDescription(
+      "Stops the TTS playback and clears the queue. (used for when she gets stuck)",
+    ),
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    await interaction.deferReply({ ephemeral: true });
+    const channel = interaction.member?.voice.channel;
+    if (!channel) {
+      await interaction.editReply(
+        "You need to be in a voice channel to use this command.",
+      );
+      return;
+    }
+    // get the player for the current guild        const player = channel.player;
+    if (!channel.player) {
+      await interaction.editReply(
+        "I'm not currently playing anything in your voice channel.",
+      );
+      return;
+    }
+
+    channel.player.forceStop();
+    await interaction.editReply("TTS playback stopped and queue cleared.");
+  },
+};
+
+export default command;
