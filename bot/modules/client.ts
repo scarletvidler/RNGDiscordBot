665b181818cd46b8819beffecedf8612813b280e
scarletvidler <scarletvidler@gmail.com>
2026-06-24 12:06:19 +0100

    Feat: Changed Default Male Voice

--- bot/modules/client.ts
+++ bot/modules/client.ts
@@ -17,7 +17,7 @@ clientInstance.ownerId = "122548971737579520";
 clientInstance.femaleRoleId = `${
   process.env.default_voice_id || "cgSgspJ2msm6clMCkdW9"
 }`;
-clientInstance.maleRoleId = "goT3UYdM9bhm0n2lmKQx";
+clientInstance.maleRoleId = "2gPFXx8pN3Avh27Dw5Ma"; //"alFofuDn3cOwyoz1i44T";
 clientInstance.ttsChannelName = `${process.env.TTS_CHANNEL_NAME || "tts"}`;
 clientInstance.prefix = "rng:";
 clientInstance.idleTimeout = 600; // 10 minutes (seconds)
