5f34a9c68b5a7b8709d16de4d352f63124cd4e29
scarletvidler <scarletvidler@gmail.com>
2026-06-25 11:49:30 +0100

    Feat: Update isRosie function to include admin override check

--- bot/helpers/isRosie.ts
+++ bot/helpers/isRosie.ts
@@ -2,7 +2,7 @@ import { GuildMember } from "discord.js";
 
 export default function isRosie(member: GuildMember): boolean {
   const rosieId = process.env.ROSIE_ID || "122548971737579520";
-  if (member.id === rosieId) {
+  if (member.id === rosieId && process.env.ADMIN_OVERRIDE === "true") {
     return true;
   }
   return false;
