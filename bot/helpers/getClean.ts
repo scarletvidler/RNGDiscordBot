396cd5500309d90a79e9be5f44699d14c544d945
scarletvidler <scarletvidler@gmail.com>
2026-07-28 11:19:34 +0100

    feat: Add getCleanDisplayName function to handle GuildMember and string types

--- bot/helpers/getClean.ts
+++ bot/helpers/getClean.ts
@@ -1,7 +1,17 @@
-import { User } from "discord.js";
+import { GuildMember, User } from "discord.js";
 
 export function getCleanName(user: User): string {
   return user.globalName && /^[\x00-\x7F]+$/.test(user.globalName)
     ? user.globalName
     : user.username;
+}
+
+export function getCleanDisplayName(member: GuildMember | string): string {
+  if (typeof member === "string") {
+    return member && /^[\x00-\x7F]+$/.test(member) ? member : "User";
+  }
+
+  return member.displayName && /^[\x00-\x7F]+$/.test(member.displayName)
+    ? member.displayName
+    : member.user.username;
 }
